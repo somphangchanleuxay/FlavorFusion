42216db564630eb8c15692d41616ad8828135a01
cammichbursc@gmail.com <cammichbursc@gmail.com>
2023-12-07 22:06:23 -0600

    added Yelp API call

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
-function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'http://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response();
-      })
-      .then(function (data) {
-        console.log(data)
-      })
 
-    };
+function getApi (){
+
+var requestUrl = 'https://www.themealdb.com/api/json/v2/9973533/filter.php?i=chicken_breast,garlic,salt';
+  
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+     })
+}
+
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch('https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
