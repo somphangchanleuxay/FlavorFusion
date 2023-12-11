ba7e526556268d75712a9f8c55ad941e8ddc0c4d
Ty Hall <ty.hallr@gmail.com>
2023-12-10 22:42:06 -0500

    Added input box etc

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,29 @@ fetch('https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', o
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
+
+  // The following items are for the pantry form data
+
+var pantryFormEl = $('#pantry-form');
+var pantryListEl = $('#pantry-list');
+var pantryClListEl = $('input[name="pantry-input"]');
+
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  
+  console.log('Pantry Item:', pantryClListEl.val());
+
+  var pantryItem = $('input[name="pantry-input"]').val();
+  if (!pantryItem) {
+    console.log('No pantry item entered!');
+    return;
+  }
+
+  pantryListEl.append('<li>' + pantryItem + '</li>');
+
+  $('input[name="pantry-input"]').val('');
+}
+
+pantryFormEl.on('submit', handleFormSubmit);
