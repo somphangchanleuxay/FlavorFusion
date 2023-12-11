e56253112ad399d58bf64d5458c38a13cc74f079
cbursch13 <147578974+cbursch13@users.noreply.github.com>
2023-12-10 21:46:55 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,6 @@ const options = {method: 'GET', headers: {accept: 'application/json'}};
 fetch('https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
   .then(response => response.json())
   .then(response => console.log(response))
-  .catch(err => console.error(err));
 
   // The following items are for the pantry form data
 
